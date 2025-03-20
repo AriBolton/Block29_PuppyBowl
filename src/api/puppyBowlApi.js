75211d860248eb7b6f73ab77fe1a475b9fdd78e7
aribolton <ari.bolton2011@gmail.com>
2025-03-20 16:28:46 -0700

    test

--- src/api/puppyBowlApi.js
+++ src/api/puppyBowlApi.js
@@ -9,7 +9,7 @@ export const puppyBowlApi = createApi({
     // Define a base query function that all endpoints will use as the base of their request
     baseQuery: fetchBaseQuery({
         // The base URL for all requests
-        baseUrl: "https://fsa-puppy-bowl.herokuapp.com/api/2411-ftb-et-web-pt-ab",
+        baseUrl: "https://fsa-puppy-bowl.herokuapp.com/api/2411-ftb-et-web-pt-amb",
     }),
     // Define endpoints for our API service
     endpoints: (builder) => ({
